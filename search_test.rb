886a39f16f0de3dd766861b909c83832debceccc
jerry <jerry@jerrys-MBP.attlocal.net>
2020-04-09 18:19:10 -0400

    Add different api source and param

--- search_test.rb
+++ search_test.rb
@@ -11,11 +11,10 @@ class PlantSearchTest < Test::Unit::TestCase
     PlantSearch
   end
 
-	def test_it_says_hello_world
-		correct = '[{"common_name":"rosemary","complete_data":false,"id":175919,"link":"http://trefle.io/api/plants/175919","scientific_name":"Rosmarinus","slug":"rosmarinus"},{"common_name":"rosemary","complete_data":true,"id":175920,"link":"http://trefle.io/api/plants/175920","scientific_name":"Rosmarinus officinalis","slug":"rosmarinus-officinalis"}]'
-	
-    get '/'
+	def test_basic_api_call
+    get '/api/tomato'
+
 			assert last_response.ok?
-    	assert_equal correct, last_response.body
+    	assert  last_response.body.include?('tomato')
   end
 end
